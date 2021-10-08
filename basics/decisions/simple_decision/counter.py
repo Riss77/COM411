566ec1dfe786213711d352e03e26d06f5173feb6
Riss77 <5jackm11@solent.ac.uk>
2021-10-08 14:41:22 +0100

    Counter added and printed

---
+++ basics/decisions/simple_decision/counter.py
@@ -0,0 +1,35 @@
+
+odds = 0
+evens = 0
+
+print("\n Please enter your first whole number\n")
+
+first_number = int(input())
+
+if first_number % 2 != 1:
+    evens += 1
+else:
+    odds += 1
+
+
+print("\n Please enter your second whole number\n")
+
+second_number = int(input())
+
+if second_number % 2 != 1:
+    evens += 1
+else:
+    odds += 1
+
+print("\n Please enter your third whole number\n")
+
+third_number = int(input())
+
+if third_number % 2 != 1:
+    evens += 1
+else:
+    odds += 1
+
+
+print(evens)
+print(odds)
