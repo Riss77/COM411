e48d00cc6e71955e9b7304f32f309afb70a7b6aa
Riss77 <5jackm11@solent.ac.uk>
2021-10-08 11:39:40 +0100

    Added if statement

---
+++ basics/decisions/simple_decision/if_.py
@@ -0,0 +1,10 @@
+
+# ask user for input of book type
+print("What type of book is this?\n")
+book = input()
+# check if type matches adventure
+if book =="adventure":
+    # if true print statement
+    print("\nI like adventure books!\n")
+# print final statement
+print("Finished reading book.")
