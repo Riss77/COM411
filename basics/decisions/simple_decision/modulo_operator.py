3d7152b7915ffa020507862edf50cae6e14b7696
Riss77 <5jackm11@solent.ac.uk>
2021-10-08 12:47:57 +0100

    Modulo_operator

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,9 @@
+
+print("\nPlease enter a whole number.\n")
+
+number = int(input())
+
+if number % 2 != 1:
+    print("\nYou have chosen an even number.\n")
+else:
+    print("\nYou have chosen an odd number.\n")
