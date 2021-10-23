cccc6b0926e182974c3499ebe80d39345c344f35
Riss77 <5jackm11@solent.ac.uk>
2021-10-23 15:41:56 +0100

    Added code to demonstrate the use of a simple for loop.

---
+++ basics/repititions/for_loop/simple.py
@@ -0,0 +1,10 @@
+
+mountains = int(input("How many mountains should I display?\n"))
+
+print("Displaying...\n")
+
+for count in range(0, mountains, 1):
+    print("      ^     ")
+    print("     /  \   ")
+    print("    /    \   ")
+    print("   /      \  ")
---
+++ basics/repititions/while_loop/factorial.py
@@ -0,0 +1,11 @@
+
+number = int(input("Please enter a number:"))
+
+count = 0
+sum_numbers = 0
+
+while count < number:
+    sum_numbers * (count * number)
+    count += 1
+
+print(f"The factorial is {sum_numbers}")
