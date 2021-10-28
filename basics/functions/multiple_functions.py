8ff7c63f90e8e92ffdeee28eb907f004539fc02d
Riss77 <5jackm11@solent.ac.uk>
2021-10-28 15:22:09 +0100

    Added code to demonstrate the implementation of multiple user defined functions.

---
+++ basics/functions/multiple_functions.py
@@ -0,0 +1,13 @@
+def display_ladder(steps):
+    for step in range(steps):
+        print("| |")
+        print("***")
+
+    print("| |")
+
+
+def create_ladder():
+    steps = int(input("How many steps remain?\n"))
+    display_ladder(steps)
+
+create_ladder()
