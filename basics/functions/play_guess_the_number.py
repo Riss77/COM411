affb0e6e10d240d1868d724dadbc564ebbf2d26f
Riss77 <5jackm11@solent.ac.uk>
2021-11-01 21:01:06 +0000

    Added guess the number as a user defined function

---
+++ basics/functions/play_guess_the_number.py
@@ -0,0 +1,21 @@
+def play_guess_the_number():
+    import random
+
+    min_val = int(input("Please enter the minimum value:\n"))
+    max_val = int(input("Please enter the maximum value:\n"))
+
+    value = random.randrange(min_val, max_val)
+    print(f"I am thinking of a number between {min_val} and {max_val}.\n Can you guess what it is?\n")
+
+    while True:
+        guess = int(input("Please enter a number:\n"))
+
+        if guess == value:
+            print("You guessed correctly!")
+            break
+        elif guess < value:
+            print("Your guess is too low, please try again.\n")
+        else:
+            print("Your guess is too high, please try again.\n")
+
+    print("Game Over!")
