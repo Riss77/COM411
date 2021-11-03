d740242eb5ec87cd66d927dba10de56247d8fa49
Riss77 <5jackm11@solent.ac.uk>
2021-11-03 01:52:24 +0000

    Titanic activity

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -21,5 +21,20 @@ def run():
     print(f"Successfully loaded {num_records} records.")
 
 
+def display_menu():
+    print(
+        """
+        Please select one of the following options:
+        [1] Display the names of all passengers
+        [2] Display the number of passengers that survived
+        [3] Display the number of passengers per gender
+        [4] Display the number of passengers per age group
+        [5] Display the number of survivors per age group"
+      
+        """)
+
+    return int(input())
+
+
 if __name__ == "__main__":
     run()
