a8453818176dfe7ffcb6f00eb3942ab43394a11e
Riss77 <5jackm11@solent.ac.uk>
2021-11-19 11:54:39 +0000

    Added code to display contents of a CSV file in subplots.

---
+++ visual/subplots/week_temps.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import csv
+
+
+def read_data():
+    with open("temps.csv") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        header = next(csv_reader)
+        data = {'week 1':[], 'week 2':[]}
+
+        for values in csv_reader:
+            data['week 1'].append(values[0])
+            data['week 2'].append(values[1])
+
+    return data
+
+
+def run():
+    data = read_data()
+    fig, axs = plt.subplots(2, 1, sharex="col")
+
+    days = range(1, 8)
+
+    axs[0].plot(days, data['week 1'])
+    axs[1].plot(days, data['week 2'])
+
+    plt.tight_layout()
+    plt.show()
+
+
+run()
